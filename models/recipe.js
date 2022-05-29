3cee75cb7b2f355dc640b085370d429b2da6809c
alec <abacqu0716@gmail.com>
2022-05-29 13:21:35 -0500

    updated model

--- models/recipe.js
+++ models/recipe.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
 	title: { type: String, required: true },
-	detail: { type: String, required: true },
+	ingredients: { type: String, required: true },
+	steps: { type: String, required: true },
     img: { type: String},
 	completed: Boolean,
 });
